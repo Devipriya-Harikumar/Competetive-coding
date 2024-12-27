a7bd90e036aefc1e7360048bbab93475838aa9bc
Devipriya-Harikumar <43454187+Devipriya-Harikumar@users.noreply.github.com>
2024-12-27 16:19:20 +0530

    Time: 15 ms (11.87%) | Memory: 45.8 MB (30.64%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,33 @@
+class Solution {
+    HashMap<Integer,List<Integer>> hm;
+    Set<Integer> visited = new HashSet<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        hm = new HashMap<>();
+        for(int[] pre : prerequisites) {
+            List<Integer> list = hm.getOrDefault(pre[0], new ArrayList<>());
+            list.add(pre[1]);
+            hm.put(pre[0], list);
+        }
+        System.out.print(hm.toString());
+        for(int i =0; i<numCourses; i++){
+            if(!dfs(i)) return false;
+        }
+        return true;
+    }
+
+    private boolean dfs(int course){
+        if(visited.contains(course)){
+            return false;
+        }
+        if(!hm.containsKey(course)){
+            return true;
+        }
+        visited.add(course);
+        for(int i : hm.get(course)){
+            if(!dfs(i)) return false;
+        }
+        visited.remove(course);
+        hm.remove(course);
+        return true;
+    }
+}
