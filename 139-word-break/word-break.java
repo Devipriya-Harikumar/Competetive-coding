3f24d7dfae0556e02b0939f2db4c75c2899612bd
Devipriya-Harikumar <43454187+Devipriya-Harikumar@users.noreply.github.com>
2024-12-30 11:18:22 +0530

    Time: 3 ms (80.76%) | Memory: 41.8 MB (89.58%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,33 +1,21 @@
 class Solution {
-    private String s;
-    private List<String> wordDict;
-    private int[] memo;
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        this.s = s;
-        this.wordDict = wordDict;
-        this.memo = new int[s.length()];
-        Arrays.fill(this.memo, -1);
-        return dp(s.length() - 1);
-    }
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (String word : wordDict) {
+                // Handle out of bounds case
+                if (i < word.length() - 1) {
+                    continue;
+                }
 
-    private boolean dp(int i) {
-        if (i < 0)
-            return true;
-        if (memo[i] != -1) {
-            return memo[i] == 1;
-        }
-        for (String word : wordDict) {
-            if (i - word.length() + 1 < 0) {
-                continue;
-            }
-            if (s.substring(i - word.length() + 1, i + 1).equals(word) &&
-                    dp(i - word.length())) {
-                memo[i] = 1;
-                return true;
+                if (i == word.length() - 1 || dp[i - word.length()]) {
+                    if (s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
             }
         }
-        memo[i] = 0;
-        return false;
+        return dp[s.length() - 1];
     }
 }
