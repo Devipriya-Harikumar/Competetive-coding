c365fec11de2b3c3190d53ec634c0c84dc73006a
Devipriya-Harikumar <43454187+Devipriya-Harikumar@users.noreply.github.com>
2025-01-24 01:28:37 +0530

    Time: 21 ms (58.05%) | Memory: 46.2 MB (19.09%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,25 +1,35 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-         int[] counter = new int[26];
-        int max = 0;
-        int maxCount = 0;
-        for(char task : tasks) {
-            counter[task - 'A']++;
-            if(max == counter[task - 'A']) {
-                maxCount++;
-            }
-            else if(max < counter[task - 'A']) {
-                max = counter[task - 'A'];
-                maxCount = 1;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+
+        int[] freq = new int[26];
+        for (char c : tasks) {
+            freq[c - 'A']++;
+        }
+
+        for(int i = 0; i<26; i++){
+            if(freq[i]>0){
+                 maxHeap.add(freq[i]);
             }
         }
-        
-        int partCount = max - 1;
-        int partLength = n - (maxCount - 1);
-        int emptySlots = partCount * partLength;
-        int availableTasks = tasks.length - max * maxCount;
-        int idles = Math.max(0, emptySlots - availableTasks);
-        
-        return tasks.length + idles;
+
+        int time = 0;
+
+        while(!maxHeap.isEmpty()) {
+            int cycle = n+1;
+            List<Integer> store = new ArrayList<>();
+            int taskCount = 0;
+
+            while(cycle-- >0 && !maxHeap.isEmpty()){
+                int currentFreq = maxHeap.poll();
+                if(currentFreq>1){
+                    store.add(currentFreq-1);
+                }
+                taskCount++;
+            }
+            store.forEach(maxHeap::add);
+            time += (maxHeap.isEmpty())?taskCount:n+1;
+        }
+        return time;
     }
 }
