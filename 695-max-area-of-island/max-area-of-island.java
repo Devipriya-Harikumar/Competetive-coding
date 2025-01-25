681ec930eca1cace8e52dd8dca639b2202ac5924
Devipriya-Harikumar <43454187+Devipriya-Harikumar@users.noreply.github.com>
2025-01-25 18:44:54 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (59.81%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -2,42 +2,28 @@ class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
-        List<String> visited = new ArrayList<>();
-        int[][] directions = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
         int maxArea = 0;
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 1 && !visited.contains(i + "," + j)) {
+                if (grid[i][j] == 1 ) {
                     maxArea = Math.max(maxArea,
-                            bfs(grid, row, col, i, j, visited, directions));
+                            dfs(grid, i,j,row, col));
                 }
             }
         }
         return maxArea;
     }
 
-    private int bfs(int[][] grid, int row, int col, int r, int c, List<String> visited, int[][] directions) {
-        int count = 1;
-        visited.add(r + "," + c);
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] { r, c });
-
-        while (!q.isEmpty()) {
-            int[] p = q.poll();
-            for (int[] point : directions) {
-                int nr = p[0] + point[0];
-                int nc = p[1] + point[1];
-                if (nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == 1
-                        && !visited.contains(nr + "," + nc)) {
-                    count++;
-                    q.add(new int[] { nr, nc });
-                    visited.add(nr + "," + nc);
-                }
-
-            }
+    private int dfs(int[][] grid, int r, int c, int row, int col){
+        if(r<0 || r>=row || c<0 || c>=col || grid[r][c] != 1){
+            return 0;
+        } else {
+            grid[r][c] = 2;
+            return 1 + dfs(grid, r+1, c, row, col)+
+            dfs(grid, r-1, c, row, col)+
+            dfs(grid, r, c+1, row, col)+
+            dfs(grid, r, c-1, row, col);
         }
-
-        return count;
     }
 }
