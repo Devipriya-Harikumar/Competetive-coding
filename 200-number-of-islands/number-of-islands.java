c847112b22cb9b6b2f1cac445915e72450547766
Devipriya-Harikumar <43454187+Devipriya-Harikumar@users.noreply.github.com>
2025-01-25 18:35:28 +0530

    Time: 48 ms (5.19%) | Memory: 57 MB (5.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -5,13 +5,11 @@ class Solution {
         int cols = grid[0].length;
         Set<String> visited = new HashSet<>();
 
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 if (grid[r][c] == '1' && !visited.contains(r + "," + c)) {
                     islands++;
-                    dfs(grid, r, c, visited, directions, rows, cols);
+                    dfs(grid, r, c, visited, rows, cols);
                 }
             }
         }
@@ -19,13 +17,13 @@ class Solution {
         return islands;        
     }
 
-     private void dfs(char[][] grid, int r, int c, Set<String> visited, int[][] directions, int rows, int cols) {
-            visited.add(r + "," + c);
-            for (int[] direction : directions) {
-                int nr = r + direction[0], nc = c + direction[1];
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && !visited.contains(nr + "," + nc)) {
-                  dfs(grid, nr, nc, visited, directions, rows, cols);
-                }
+     private void dfs(char[][] grid, int r, int c, Set<String> visited, int rows, int cols) {
+                if (r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == '1' && !visited.contains(r + "," + c)) {
+                    visited.add(r + "," + c);
+                  dfs(grid, r+1, c, visited, rows, cols);
+                  dfs(grid, r-1, c, visited, rows, cols);
+                  dfs(grid, r, c+1, visited, rows, cols);
+                  dfs(grid, r, c-1, visited, rows, cols);
             }
     }
 }
