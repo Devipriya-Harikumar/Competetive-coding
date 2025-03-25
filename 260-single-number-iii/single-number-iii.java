87045882c81726997891888ba377a516ccb2c2f2
Devipriya-Harikumar <43454187+Devipriya-Harikumar@users.noreply.github.com>
2025-03-25 08:08:12 +0530

    Time: 4 ms (42.79%) | Memory: 46.2 MB (7.86%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        System.out.print(nums);
+        Arrays.sort(nums);
+        System.out.print(nums);
+        int index = 0;
+        int[] result = new int[2];
+        for(int i = 0; i< nums.length -1; i++){
+            if(nums[i] != nums[i+1]){
+                result[index] = nums[i];
+                index++;
+            } else {
+                i++;
+            }
+        }
+        if(index == 1){
+                result[index] = nums[nums.length -1];
+            }
+        return result;
+    }
+}
