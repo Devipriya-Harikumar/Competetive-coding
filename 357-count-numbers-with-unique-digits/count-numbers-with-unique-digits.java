89818f0fe3e85efcf902d0199b815fe4d67d228e
Devipriya-Harikumar <43454187+Devipriya-Harikumar@users.noreply.github.com>
2025-03-31 00:35:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (27.95%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    int[] result = new int[8];
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n == 0) return 1;
+        int ans = 10;
+        int start = 9;
+        int current = 9;
+        while(n-- > 1 && start>0){
+            current *= (start--);
+            ans += current;
+        }
+        return ans;
+    }
+}
